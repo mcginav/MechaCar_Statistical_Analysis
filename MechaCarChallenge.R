a09db1ecf9684f193266ac86af3078b601574c19
mcginav <74389679+mcginav@users.noreply.github.com>
2021-03-13 12:33:07 -0800

    Updating script for Del 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,17 @@ f2<- file.choose()
 MechCar2 <- read.csv(file=f2, check.names = F, stringsAsFactors = F)
 total_summary <- MechCar2 %>%  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table
 lot_summary <- MechCar2 %>% group_by(Manufacturing_Lot) %>%  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table
+
+?t.test()
+
+# t test for all lots
+t.test(MechCar2$PSI,Mean=mean(MechCar2$PSI))
+
+# t test for lot 1
+t.test(subset(MechCar2$PSI,MechCar2$Manufacturing_Lot == "Lot1"),Mean=mean(MechCar2$PSI))
+
+# t test for lot 2
+t.test(subset(MechCar2$PSI,MechCar2$Manufacturing_Lot == "Lot2"),Mean=mean(MechCar2$PSI))
+
+# t test for lot 3
+t.test(subset(MechCar2$PSI,MechCar2$Manufacturing_Lot == "Lot3"),Mean=mean(MechCar2$PSI))
