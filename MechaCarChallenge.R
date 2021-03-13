085f42f7748d8f3368c48d864d14a58424e69e7e
mcginav <74389679+mcginav@users.noreply.github.com>
2021-03-13 11:39:22 -0800

    Adding screenshots and script for Del 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,11 @@
 library(dplyr)
 f <- file.choose()
 MechaCarChallenge <- read.csv(file=f, check.names = F,stringsAsFactors = F)
+?lm()
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCarChallenge)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCarChallenge))
+
+f2<- file.choose()
+MechCar2 <- read.csv(file=f2, check.names = F, stringsAsFactors = F)
+total_summary <- MechCar2 %>%  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table
+lot_summary <- MechCar2 %>% group_by(Manufacturing_Lot) %>%  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table
